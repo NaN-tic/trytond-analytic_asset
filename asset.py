dbcc9a08e1f3b9ebc8330b8aef9131517268113c
Albert Cervera i Areny <albert@nan-tic.com>
2016-05-25 22:57:09 +0200

    Ensure company field can be set.

--- asset.py
+++ asset.py
@@ -1,6 +1,7 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.pool import PoolMeta
+from trytond.pool import PoolMeta, Pool
+from trytond.model import fields
 
 from trytond.modules.analytic_account import AnalyticMixin
 
@@ -20,3 +21,21 @@ class AnalyticAccountEntry:
     def _get_origin(cls):
         origins = super(AnalyticAccountEntry, cls)._get_origin()
         return origins + ['asset']
+
+    @fields.depends('origin')
+    def on_change_with_company(self, name=None):
+        Asset = Pool().get('asset')
+        company = super(AnalyticAccountEntry, self).on_change_with_company(
+            name)
+        if isinstance(self.origin, Asset):
+            if self.origin.company:
+                return self.origin.company.id
+        return company
+
+    @classmethod
+    def search_company(cls, name, clause):
+        domain = super(AnalyticAccountEntry, cls).search_company(name, clause)
+        domain = ['OR',
+            domain,
+            [('origin.company',) + tuple(clause[1:]) + tuple(('asset',))]]
+        return domain
