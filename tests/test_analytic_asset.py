95b358a4fee84409a7d1ec830f2d4e3174c3bde8
Guillem Barba <guillem@nan-tic.com>
2016-10-06 15:21:18 +0200

    Adapt tests and __init__ to new style

--- tests/test_analytic_asset.py
+++ tests/test_analytic_asset.py
@@ -5,12 +5,13 @@ import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(ModuleTestCase):
-    'Test module'
+class AnalyticAssetTestCase(ModuleTestCase):
+    'Test AnalyticAsset module'
     module = 'analytic_asset'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+            AnalyticAssetTestCase))
     return suite
